0350fd3c2213e7819ba40a652fd63061c8bb161b
Mark Boleigha <donhoenix@gmail.com>
2020-07-23 16:19:12 +0100

    first commmit

---
+++ UserStories/UserStories.h
@@ -0,0 +1,19 @@
+//
+//  UserStories.h
+//  UserStories
+//
+//  Created by Sprinthub on 23/07/2020.
+//  Copyright © 2020 Sprinthub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserStories.
+FOUNDATION_EXPORT double UserStoriesVersionNumber;
+
+//! Project version string for UserStories.
+FOUNDATION_EXPORT const unsigned char UserStoriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserStories/PublicHeader.h>
+
+
